ac605faf8d1b37d12be93641615e8630c89cd524
Eberson Silva <ebersonsilva380@gmail.com>
2025-02-20 07:06:46 -0300

    desafio de mudar a frase junto com a foto no javaScript cocluido

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ function toggleMode() {
 
   //pegar a tag img
   const img = document.querySelector("#profile img")
-  // substituir a imagem
+  // substituir a imagem e frase
   if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a imagem light
+    // se tiver light mode, adicionar a imagem light e frase light
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "foto de eberson silva com a mão no queixo e fundo de grama verde atrás"
+    )
   }
-  // se tiver sem light mode, manter a imagem normal
+  // se tiver sem light mode, manter a imagem normal e frase também
   else {
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "foto de eberson silva olhando para a sua esquerda com uma parede branca atrás ebersonsilva"
+    )
   }
 }
